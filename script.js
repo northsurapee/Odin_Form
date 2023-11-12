e4f250d631b171cc1bb089eac6fed3d082938c8f
Surapee <suwan.srp@gmail.com>
2023-11-12 22:56:26 +0700

    Edit the html and css for small screen

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Reference Variables
-const submitBtn = document.querySelector('.submit-btn');
 const password = document.querySelector('.password');
 const passwordConfirm = document.querySelector('.confirm_password');
 const errorDisplayers = document.querySelector('.error');
@@ -8,15 +7,16 @@ const errorDisplayers = document.querySelector('.error');
 passwordConfirm.addEventListener('keyup', isPasswordMatch);
 password.addEventListener('keyup', isPasswordMatch);
 
+// Match the password and confirm password fields in a form as the user types
 function isPasswordMatch() {
     let pass = password.value;  // Use .value instead of .textContent for input elements
     let confirmPass = passwordConfirm.value;
 
-    if (pass !== confirmPass) {  // Use strict inequality operator !==
+    if (pass !== confirmPass) {  // Not match : .error text will appear
         errorDisplayers.textContent = '* Passwords do not match';
         password.style.borderColor = 'red';
         passwordConfirm.style.borderColor = 'red';  // Add this line to highlight confirm password field
-    } else {
+    } else { // match : .error text dissappear
         errorDisplayers.textContent = '';
         password.style.borderColor = '';
         passwordConfirm.style.borderColor = '';  // Reset confirm password field border color
